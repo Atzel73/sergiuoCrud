64f038991a0c451f3cbf97d591089ca7d45a265c
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-05-07 13:46:32 -0600

    pantallas

--- App.js
+++ App.js
@@ -13,10 +13,36 @@ const Stack = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Login}/>
-        <Stack.Screen name="Users" component={Users}/>
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'animales') {
+              iconName = focused ? 'pets' : 'snippet-folder';
+            } else if (route.name === 'ALimentos') {
+              iconName = focused ? 'pets' : 'snippet-folder';
+            } else if (route.name === 'Practicas') {
+              iconName = focused ? 'pets' : 'snippet-folder'; 
+            } else if (route.name === 'Functions') {
+              iconName = focused ? 'pets' : 'snippet-folder';
+            }
+
+            return <MaterialIcons name={iconName} size={24} color="black" />
+          },
+          tabBarActiveTintColod: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        
+        >
+          <Tab.Screen name="Animales" component={AdminGroup} />
+          <Tab.Screen name="Alimentos" component={Aliments} />
+          {/*<Tab.Screen name="Practicas" component={AnimationScreen} />
+          <Tab.Screen name="Functions" component={InfoScreen} />*/}
+        </Tab.Navigator>
+        {/* <Stack.Navigator headerMode="none">
+        <Stack.Screen name="Listas" component={Login} />
+        </Stack.Navigator>*/}
     </NavigationContainer>
   );
 }
