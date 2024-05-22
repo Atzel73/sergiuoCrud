d07f7d8ab56f1825f97c0b6c65af9e416ed108ef
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-05-21 21:37:36 -0600

    actualizaciones

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 import Login from './screens/login';
 import Users from './screens/users'
 
-const Stack = createBottomTabNavigator();
+const Stack = createStackNavigator();
+// const Top = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -23,7 +27,7 @@ export default function App() {
             } else if (route.name === 'ALimentos') {
               iconName = focused ? 'pets' : 'snippet-folder';
             } else if (route.name === 'Practicas') {
-              iconName = focused ? 'pets' : 'snippet-folder'; 
+              iconName = focused ? 'pets' : 'snippet-folder';
             } else if (route.name === 'Functions') {
               iconName = focused ? 'pets' : 'snippet-folder';
             }
@@ -33,14 +37,14 @@ export default function App() {
           tabBarActiveTintColod: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}
-        
-        >
-          <Tab.Screen name="Animales" component={AdminGroup} />
-          <Tab.Screen name="Alimentos" component={Aliments} />
-          {/*<Tab.Screen name="Practicas" component={AnimationScreen} />
+
+      >
+        <Tab.Screen name="Animales" component={Login} />
+        <Tab.Screen name="Alimentos" component={Users} />
+        {/*<Tab.Screen name="Practicas" component={AnimationScreen} />
           <Tab.Screen name="Functions" component={InfoScreen} />*/}
-        </Tab.Navigator>
-        {/* <Stack.Navigator headerMode="none">
+      </Tab.Navigator>
+      {/* <Stack.Navigator headerMode="none">
         <Stack.Screen name="Listas" component={Login} />
         </Stack.Navigator>*/}
     </NavigationContainer>
