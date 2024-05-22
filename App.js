e3706c4b58c2afe95361ce0da78d2a38e458d441
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-05-21 22:17:58 -0600

    estilos

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import 'react-native-gesture-handler';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 import Login from './screens/login';
-import Users from './screens/users'
+import Users from './screens/users';
 
 const Stack = createStackNavigator();
-// const Top = createMaterialTopTabNavigator();
+const Top = createMaterialTopTabNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -22,32 +22,22 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'animales') {
+            if (route.name === 'Login') {
               iconName = focused ? 'pets' : 'snippet-folder';
-            } else if (route.name === 'ALimentos') {
-              iconName = focused ? 'pets' : 'snippet-folder';
-            } else if (route.name === 'Practicas') {
-              iconName = focused ? 'pets' : 'snippet-folder';
-            } else if (route.name === 'Functions') {
+            } else if (route.name === 'Users') {
               iconName = focused ? 'pets' : 'snippet-folder';
             }
 
-            return <MaterialIcons name={iconName} size={24} color="black" />
+            // Return the MaterialIcons component with the appropriate icon name, size, and color
+            return <MaterialIcons name={iconName} size={24} color={color} />;
           },
-          tabBarActiveTintColod: 'tomato',
+          tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}
-
       >
-        <Tab.Screen name="Animales" component={Login} />
-        <Tab.Screen name="Alimentos" component={Users} />
-        {/*<Tab.Screen name="Practicas" component={AnimationScreen} />
-          <Tab.Screen name="Functions" component={InfoScreen} />*/}
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="Users" component={Users} />
       </Tab.Navigator>
-      {/* <Stack.Navigator headerMode="none">
-        <Stack.Screen name="Listas" component={Login} />
-        </Stack.Navigator>*/}
     </NavigationContainer>
   );
 }
-
--- screens/users.js
+++ screens/users.js
@@ -9,31 +9,31 @@ export default function Users() {
 
     const [users, setUsersData] = useState([]);
 
-    useEffect(() => {
-        const getData = () => {
-            const q = query(collection(db, "users"));
-            const arrayEmpty = [];
+    // useEffect(() => {
+    //     const getData = () => {
+    //         const q = query(collection(db, "users"));
+    //         const arrayEmpty = [];
 
-            const sendData = onSnapshot(q, (querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    arrayEmpty.push(doc.data());
-                    console.log("Datos: ", doc.data());
-                });
-                setUsersData(arrayEmpty);
-            });
-        };
-        getData();
-    }, []);
+    //         const sendData = onSnapshot(q, (querySnapshot) => {
+    //             querySnapshot.forEach((doc) => {
+    //                 arrayEmpty.push(doc.data());
+    //                 console.log("Datos: ", doc.data());
+    //             });
+    //             setUsersData(arrayEmpty);
+    //         });
+    //     };
+    //     getData();
+    // }, []);
 
     return(
         <View>
-            {users.map((item, index) => {
+            {/* {users.map((item, index) => {
                 return (
                     <View key={index}>
                         <Text> Email: {item.email}</Text>
                     </View>
                 );
-            })}
+            })} */}
         </View>
     );
 }
